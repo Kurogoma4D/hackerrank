7e42e8a6014d97c405daaa52cf4e1ad4648c16f4
Takafumi Suzuki <highdro0479@gmail.com>
2019-11-27 16:57:17 +0900

    first commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# これは何？
+[HackerRank](https://www.hackerrank.com/) の練習問題などを解く場所
+
+C++の練習も兼ねて，基本的にC++で解いていく．
---
+++ playground.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> vec = {25, 32, 15, 43};
+    cout << vec.at(0) << "\n";
+    cout << vec.size() << "\n";
+}
