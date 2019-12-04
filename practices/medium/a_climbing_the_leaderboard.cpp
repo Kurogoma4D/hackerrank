ef9f42bf92c42c9c95118ac3e674c14108d43fa3
Takafumi Suzuki <highdro0479@gmail.com>
2019-12-04 17:27:05 +0900

    2 problems

---
+++ practices/medium/a_climbing_the_leaderboard.cpp
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the climbingLeaderboard function below.
+vector<int> climbingLeaderboard(vector<int> scores, vector<int> alice)
+{
+    int n = scores.size();
+    int m = alice.size();
+    vector<int> ranking(n, 1);
+    vector<int> alice_ranks(m, 1);
+    vector<int>::iterator rank_position;
+    vector<int> scores_reverse(n, 0);
+
+    reverse_copy(scores.begin(), scores.end(), scores_reverse.begin());
+
+    int rank = 1;
+    for (int i = 1; i < n; i++)
+    {
+        if (scores[i] < scores[i - 1])
+        {
+            rank++;
+        }
+        ranking[i] = rank;
+    }
+
+    int rank_index = 0;
+    for (int i = 0; i < alice.size(); i++)
+    {
+        rank_position = lower_bound(scores_reverse.begin(), scores_reverse.end(), alice[i]);
+        if (rank_position == scores_reverse.begin())
+        {
+            alice_ranks[i] = ranking[n - 1] + 1;
+        }
+        else if (rank_position == scores_reverse.end())
+        {
+            alice_ranks[i] = 1;
+        }
+        else
+        {
+            rank_index = distance(scores_reverse.begin(), rank_position);
+            alice_ranks[i] = ranking[n - rank_index];
+        }
+    }
+
+    return alice_ranks;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int scores_count;
+    cin >> scores_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string scores_temp_temp;
+    getline(cin, scores_temp_temp);
+
+    vector<string> scores_temp = split_string(scores_temp_temp);
+
+    vector<int> scores(scores_count);
+
+    for (int i = 0; i < scores_count; i++)
+    {
+        int scores_item = stoi(scores_temp[i]);
+
+        scores[i] = scores_item;
+    }
+
+    int alice_count;
+    cin >> alice_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string alice_temp_temp;
+    getline(cin, alice_temp_temp);
+
+    vector<string> alice_temp = split_string(alice_temp_temp);
+
+    vector<int> alice(alice_count);
+
+    for (int i = 0; i < alice_count; i++)
+    {
+        int alice_item = stoi(alice_temp[i]);
+
+        alice[i] = alice_item;
+    }
+
+    vector<int> result = climbingLeaderboard(scores, alice);
+
+    for (int i = 0; i < result.size(); i++)
+    {
+        fout << result[i];
+
+        if (i != result.size() - 1)
+        {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string)
+{
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [](const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ')
+    {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos)
+    {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
